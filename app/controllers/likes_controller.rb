b8a4e7d69dd0141e2ee98e4f2dc1d943cbbb28c0
Tech-A21 <Tech-A21@Tech-A21s-MacBook-Air.local>
2017-07-22 19:38:44 +0100

    can post feedback

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,7 @@
 class LikesController < ApplicationController
+	before_action :find_business
+	before_action :find_like, only: [:edit, :update, :destroy]
+
   
   
   def new
@@ -10,9 +13,44 @@ class LikesController < ApplicationController
   
   def create
     @like = Like.new(like_params)
+    @like.business_id = @business.id
+    @like.user_id = current_user.id
+    if @like.save
+    	redirect_to business_path(@business)
+    else
+    	render 'new'
+    end
   end
-  private like_params
+  
+  def edit
+  	
+
+  end 
+
+  def update
+  	@like = Like.find(params[:id])
+  	if @like.update(like_params)
+  		redirect_to business_path(@business)
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@like.destroy
+  	redirect_to business_path(@business)
+  end
+  private 
+  def like_params
   params.require(:like).permit(:rating, :comment)
+	end
+	def find_business
+		@business = Business.find(params[:business_id])
+	end
+	def find_like
+		@like = Like.find(params[:id])
+
+	end
 end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-	resources :businesses 
+	resources :businesses do 
+		resources :likes
+	end
 	root 'businesses#index'
   end
